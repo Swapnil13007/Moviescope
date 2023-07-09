06f64b2faa088bb27b6d9d3787cd88ecaeb5a694
Swapnil Pawar <bhanupawar007@gmail.com>
2023-07-09 14:44:11 +0530

    Play Button

--- js/details.js
+++ js/details.js
@@ -5,23 +5,24 @@ const searchBtn = document.querySelector(".searchBox__btn");
 
 searchBtn.addEventListener("click", (evt) => {
   sessionStorage.setItem("movieName", searchBox.value);
-})
+});
 
 let movId = sessionStorage.getItem("movId");
 let showId = sessionStorage.getItem("showId");
+let playUrl = "";
 let apiUrl = "";
 
 const setDetails = (data) => {
   const backImg = document.querySelector(".backdrop");
   const frontImg = document.querySelector(".frontImage__img");
+  const play = document.querySelector(".btn__play");
 
   let backUrl = `https://image.tmdb.org/t/p/w1280${data.backdrop_path}`;
   let frontUrl = `https://image.tmdb.org/t/p/w342${data.poster_path}`;
 
   if (data.backdrop_path) {
     backImg.style.backgroundImage = `url(${backUrl})`;
-  }
-  else {
+  } else {
     backImg.style.backgroundImage = `url(${frontUrl})`;
   }
 
@@ -41,7 +42,8 @@ const setDetails = (data) => {
 
   overview.innerHTML = `${data.overview}`;
 
-}
+  play.href = `${playUrl}${data.id}`;
+};
 
 const checkRestricted = async (videoId) => {
   let url = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${YtKey}&part=snippet,contentDetails,status`;
@@ -56,7 +58,7 @@ const checkRestricted = async (videoId) => {
   }
 
   return sta;
-}
+};
 
 const setVideos = (videos) => {
   const trailersContainer = document.querySelector(".Trailer__container");
@@ -66,26 +68,28 @@ const setVideos = (videos) => {
 
   // console.log(videos);
 
-  videos.forEach(video => {
+  videos.forEach((video) => {
     if (video.type == "Trailer" && trailersCount < 2) {
       trailersCount++;
 
       checkRestricted(video.key).then((res) => {
         if (!res) {
           const videoURL = `https://www.youtube.com/embed/${video.key}`;
-          const newVideo = document.createElement('iframe');
-          newVideo.classList.add('video');
+          const newVideo = document.createElement("iframe");
+          newVideo.classList.add("video");
           newVideo.id = "ifrvideo";
           newVideo.src = videoURL;
           newVideo.setAttribute("loading", "lazy");
           newVideo.setAttribute("frameborder", "0");
-          newVideo.setAttribute("allow", "autoplay; fullscreen; encrypted-media; picture-in-picture;");
+          newVideo.setAttribute(
+            "allow",
+            "autoplay; fullscreen; encrypted-media; picture-in-picture;"
+          );
           trailersContainer.prepend(newVideo);
-        }
-        else {
+        } else {
           --trailersCount;
         }
-      })
+      });
     }
 
     if (video.type == "Teaser" && teasersCount < 2) {
@@ -94,73 +98,70 @@ const setVideos = (videos) => {
       checkRestricted(video.key).then((res) => {
         if (!res) {
           const videoURL = `https://www.youtube.com/embed/${video.key}`;
-          const newVideo = document.createElement('iframe');
-          newVideo.classList.add('video');
+          const newVideo = document.createElement("iframe");
+          newVideo.classList.add("video");
           newVideo.src = videoURL;
           newVideo.setAttribute("loading", "lazy");
           newVideo.setAttribute("frameborder", "0");
-          newVideo.setAttribute("allow", "autoplay; fullscreen; encrypted-media; picture-in-picture;");
+          newVideo.setAttribute(
+            "allow",
+            "autoplay; fullscreen; encrypted-media; picture-in-picture;"
+          );
           teasersContainer.prepend(newVideo);
-        }
-        else {
+        } else {
           --teasersCount;
         }
-      })
+      });
     }
-
   });
 
   setTimeout(() => {
     if (trailersCount + teasersCount == 0) {
-      const vidSection = document.querySelector(".videos")
+      const vidSection = document.querySelector(".videos");
       vidSection.style.display = "none";
-    }
-    else if (trailersCount == 0) {
+    } else if (trailersCount == 0) {
       const trailerHeader = document.querySelector(".Trailers__header-box");
       trailerHeader.style.display = "none";
       trailersContainer.style.display = "none";
-    }
-    else if (teasersCount == 0) {
+    } else if (teasersCount == 0) {
       const teaserHeader = document.querySelector(".Teasers__header-box");
       teaserHeader.style.display = "none";
       teasersContainer.style.display = "none";
     }
   }, 500);
-
-}
+};
 
 const getPeopleInfo = () => {
-  const peoples = document.querySelectorAll('.Cast__img');
+  const peoples = document.querySelectorAll(".Cast__img");
 
-  peoples.forEach(people => {
+  peoples.forEach((people) => {
     people.addEventListener("click", (evt) => {
       sessionStorage.setItem("personId", evt.target.id);
-    })
-  })
-}
+    });
+  });
+};
 
 const setCast = (cast) => {
   const castContainer = document.querySelector(".Cast__container");
   let castCount = 0;
 
-  cast.forEach(people => {
+  cast.forEach((people) => {
     if (people.profile_path && castCount < 8) {
-
-      const profile = document.createElement('div');
+      const profile = document.createElement("div");
       profile.classList.add("Cast__profile");
 
-      const imgContainer = document.createElement('div');
+      const imgContainer = document.createElement("div");
       imgContainer.classList.add("Cast__img-container");
 
-      const anchor = document.createElement('a');
+      const anchor = document.createElement("a");
       anchor.href = "./people.html";
 
-      const image = document.createElement('img');
+      const image = document.createElement("img");
       image.classList.add("Cast__img");
       image.src = ` https://image.tmdb.org/t/p/w154${people.profile_path}`;
       image.id = people.id;
 
-      const name = document.createElement('div');
+      const name = document.createElement("div");
       name.classList.add("Cast__name");
       name.innerText = `${people.name}`;
 
@@ -172,7 +173,7 @@ const setCast = (cast) => {
 
       castCount++;
     }
-  })
+  });
 
   if (castCount > 0) {
     getPeopleInfo();
@@ -180,39 +181,36 @@ const setCast = (cast) => {
     const castSection = document.querySelector(".Cast__section");
     castSection.style.display = "none";
   }
-
-}
+};
 
 const getRecommendInfo = () => {
-  const images = document.querySelectorAll('.Recommended__img');
+  const images = document.querySelectorAll(".Recommended__img");
 
-  images.forEach(img => {
+  images.forEach((img) => {
     img.addEventListener("click", (evt) => {
       console.dir(evt.target);
       if (movId) {
         sessionStorage.setItem("movId", evt.target.id);
-      }
-      else {
+      } else {
         sessionStorage.setItem("showId", evt.target.id);
       }
-    })
-  })
-}
+    });
+  });
+};
 
 const setRecommedation = (data) => {
   const recommendContainer = document.querySelector(".Recommended__container");
   let recCount = 0;
 
-  data.forEach(movie => {
+  data.forEach((movie) => {
     if (movie.original_language === "en" && movie.poster_path) {
-
-      const imgContainer = document.createElement('div');
+      const imgContainer = document.createElement("div");
       imgContainer.classList.add("Recommended__img-container");
 
-      const anchor = document.createElement('a');
+      const anchor = document.createElement("a");
       anchor.href = "./details.html";
 
-      const image = document.createElement('img');
+      const image = document.createElement("img");
       image.classList.add("Recommended__img");
       image.src = ` https://image.tmdb.org/t/p/w342${movie.poster_path}`;
       image.id = movie.id;
@@ -223,7 +221,7 @@ const setRecommedation = (data) => {
 
       recCount++;
     }
-  })
+  });
 
   if (recCount > 0) {
     getRecommendInfo();
@@ -231,8 +229,7 @@ const setRecommedation = (data) => {
     const RecommendedSection = document.querySelector(".Recommended__section");
     RecommendedSection.style.display = "none";
   }
-
-}
+};
 
 const getDetails = async (url) => {
   const res = await axios.get(url);
@@ -241,17 +238,15 @@ const getDetails = async (url) => {
   setVideos(res.data.videos.results);
   setCast(res.data.credits.cast);
   setRecommedation(res.data.recommendations.results);
-}
-
+};
 
 if (movId) {
   apiUrl = `https://api.themoviedb.org/3/movie/${movId}?api_key=${APIKey}&language=en-US&append_to_response=videos,credits,recommendations,reviews`;
+  playUrl = "https://vidsrc.me/embed/movie?tmdb=";
   getDetails(apiUrl);
-}
-else if (showId) {
+} else if (showId) {
   apiUrl = `https://api.themoviedb.org/3/tv/${showId}?api_key=${APIKey}&language=en-US&append_to_response=videos,credits,recommendations,reviews`;
+  playUrl = "https://vidsrc.me/embed/tv?tmdb=";
   getDetails(apiUrl);
-}
-else {
-
+} else {
 }
